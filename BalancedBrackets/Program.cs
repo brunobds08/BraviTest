5b161dfd7db522d28e69f0917e9ef8f6894b0637
Bruno Souza <brunobds08@gmail.com>
2022-03-28 05:10:57 -0300

    Adicionar arquivos de projeto.

---
+++ BalancedBrackets/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace BraviTest.BalancedBrackets
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string brackets;
+            do
+            {
+                Console.WriteLine("Type your brackets sequence for validation or ENTER to leave...");
+                brackets = Console.ReadLine();
+
+                if (Validation.CheckBracketsSequence(brackets.ToArray()))
+                {
+                    Console.WriteLine("This is a valid sequence!");
+                }
+                else
+                {
+                    Console.WriteLine("The sequence you entered is not valid!");
+                }
+            } while (brackets != "" );
+        }
+
+
+    }
+}
---
+++ BalancedBrackets/Validation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BraviTest.BalancedBrackets
+{
+    public static class Validation
+    {
+        private static Dictionary<char, char> _bracketsPair = new Dictionary<char, char>()
+        {
+            { '[', ']' },
+            { '(', ')' },
+            { '{', '}' }
+        };
+
+        public static bool CheckBracketsSequence(char[] bracketSequence)
+        {
+            Stack<char> openingBrackets = new Stack<char>();
+            
+            foreach (var bracket in bracketSequence)
+            {
+                if (bracket == ']' || bracket == '}' || bracket == ')')
+                {
+                    if (bracket == _bracketsPair[openingBrackets.Peek()])
+                    {
+                        openingBrackets.Pop();
+                    }
+                }
+                else
+                {
+                    openingBrackets.Push(bracket);
+                }
+            }
+
+            return openingBrackets.Count == 0;
+        }
+    }
+}
